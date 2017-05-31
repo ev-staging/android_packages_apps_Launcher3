d0007c0ac769e0a1547f3caa5471792c828940e5
Amir Zaidi <azaidi@live.nl>
2017-05-31 19:57:16 +0200

    Launcher3: extend app filter
    Change-Id: I9b87f0aa09e5b4e68db0511f8250a27bf7a31ed7

--- src/com/android/launcher3/AppFilter.java
+++ src/com/android/launcher3/AppFilter.java
@@ -3,13 +3,29 @@ package com.android.launcher3;
 import android.content.ComponentName;
 import android.content.Context;
 
+import java.util.HashSet;
+
 public class AppFilter {
+    private final HashSet mHidden;
+
+    private String[] FILTERED_APPS = {
+        "com.google.android.googlequicksearchbox/.VoiceSearchActivity",
+        "com.google.android.apps.wallpaper/.picker.CategoryPickerActivity",
+        "com.google.android.launcher/com.google.android.launcher.StubApp",
+    };
+
+    public AppFilter() {
+        mHidden = new HashSet();
+        for (String pkg: FILTERED_APPS) {
+            mHidden.add(ComponentName.unflattenFromString(pkg));
+        }
+    }
 
     public static AppFilter newInstance(Context context) {
         return Utilities.getOverrideObject(AppFilter.class, context, R.string.app_filter_class);
     }
 
     public boolean shouldShowApp(ComponentName app) {
-        return true;
+        return !mHidden.contains(app);
     }
 }
